6d6c4b98c0c9d976f4e97021371e6c88628c0420
sabih-7 <sabeehahmed5@gmail.com>
2022-06-04 19:29:26 +0500

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,23 @@
+// Collapsible
+const collapsibles = document.querySelectorAll(".collapsible");
+collapsibles.forEach((item) =>
+  item.addEventListener("click", function () {
+    this.classList.toggle("collapsible--expanded");
+  })
+);
+
+//Scroll Button
+
+//Get the button
+var mybutton = document.getElementById("scrollTop");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+    mybutton.style.display = "inline-block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
