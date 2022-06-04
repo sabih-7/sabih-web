70930889ca7ff6cc3a38ef8eb791fdf978be362a
sabih-7 <89785427+sabih-7@users.noreply.github.com>
2022-06-04 20:10:10 +0500

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,19 +5,3 @@ collapsibles.forEach((item) =>
     this.classList.toggle("collapsible--expanded");
   })
 );
-
-//Scroll Button
-
-//Get the button
-var mybutton = document.getElementById("scrollTop");
-
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
-    mybutton.style.display = "inline-block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
